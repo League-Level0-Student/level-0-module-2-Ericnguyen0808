577a999a359f06a369e2e80a771903a604e88f2c
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-07 13:55:15 -0800

    WEeek 8

--- src/random/Magic8Ball.java
+++ src/random/Magic8Ball.java
@@ -11,43 +11,40 @@ import javax.swing.JOptionPane;
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below….
-private void Mainmethod() {
-	// TODO Auto-generated method stub
+	public static void main(String[] args) {
+	System.out.println(4);
+	
+
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+ 
 	
 	int r=  new Random().nextInt(4);
-	
-
-
+	int num3=new Random().nextInt(4);
 	// 3. Print out this variable
-   System.out.println(4);
+	
 	// 4. Get the user to enter a question for the 8 ball
-String Answer = JOptionPane.showInputDialog("What Question would you like to ask the Great Magic 8 Ball?");
+    String Answer = JOptionPane.showInputDialog("What Question would you like to ask the Great Magic 8 Ball?");{
 	// 5. If the random number is 0
     if(r==0) {
     JOptionPane.showMessageDialog(null, "Yes");
     }
 	// -- tell the user "Yes"
-
-	int r1 = 0;
 	// 6. If the random number is 1
-    if(r1==1) {
+    if(r==1) {
     	JOptionPane.showMessageDialog(null, "No");
     }
-    int r2 = 0;
-	if(r2==2) {
+	if(r==2) {
     	JOptionPane.showMessageDialog(null, "Maybe you should ask Google?");
     }
 	// -- tell the user "Maybe you should ask Google?"
-
-	int r3 = 0;
 	// 8. If the random number is 3
-    if(r3==3) {
+    if(r==3) {
     	JOptionPane.showMessageDialog(null, "Try again next time");
     }
 	// -- write your own answer
 }
 }
+} 
+
 
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -11,15 +11,33 @@ public class Validation {
 	public static void main(String[] args) {
 		
 		Random randomMaker = new Random();
+		for(int i=0;i<11;i++) {
 		
 		int randomNumber = randomMaker.nextInt(5);
 		
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
-		// 2. Repeat all the code above 10 times
 		
+		if(randomNumber==0) {
+        	JOptionPane.showMessageDialog(null, "You are beautiful");
+		}
+        if(randomNumber==1) {
+              	JOptionPane.showMessageDialog(null, "Get out of here and look at the mirrior you will find something nice");
+        	  }
+        if(randomNumber==2) {
+                 	JOptionPane.showMessageDialog(null, "You need to get a makeover");
+              	 }
+        if(randomNumber==3) {
+                     	JOptionPane.showMessageDialog(null, "You are godlier than the Gods."); 
+                 	 }
+        if(randomNumber==4) {
+                         	JOptionPane.showMessageDialog(null, "Wow you are more stunning than the sunflower and the sun combined");
+                     	 }
+                     	// 2. Repeat all the code above 10 times
+
 		// 3. Find someone to test out your program. They will like it :)
 	}
 }
+}
+
